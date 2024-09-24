6625267dfcf91d0c46ae04a57522d2029ebd422a
Peter Charles Otim <113760237+otimpeter@users.noreply.github.com>
2024-09-24 18:54:33 +0300

    Add files via upload
    The second commit

--- CSE - 340 WORK/script.js
+++ CSE - 340 WORK/script.js
@@ -2,19 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Set EJS as the templating engine
+// the code Set EJS as the templating engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files from the public folder
+// the Serve static files from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route for the home page
+// Route for the home page code
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-// Start the server
+// Start the server which was recommened as localhost.
 const PORT = 5500;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
